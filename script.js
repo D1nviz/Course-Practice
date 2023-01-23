c048dc2ce32175ce8635917e7f796550e92f9856
Eugen <example@gmail.com>
2023-01-23 20:10:52 +0200

    practice_02

--- script.js
+++ script.js
@@ -10,7 +10,29 @@ const personalMovieDB = {
     privat: false,
 };
 
-const lastWachedMovie = prompt("Какой ваш последний просмотреный фильм?","");
-const RateOflastWachedMovie = prompt("На сколько его оцените?","");
-personalMovieDB.movies[lastWachedMovie] = RateOflastWachedMovie;
+if(personalMovieDB.count < 10) {
+    alert("Просмотрено довольно мало фильмов");
+} else if(personalMovieDB.count < 30 || personalMovieDB.count > 10) {
+    alert("Вы классический зритель");
+} else if(personalMovieDB.count > 30) {
+    alert("Да вы киноман!");
+} else {
+    alert ("Ошибка");
+}
+
+
+
+
+for(let i = 0; i < 2; i++) {
+    let lastWachedMovie = prompt("Какой ваш последний просмотреный фильм?","");
+    let RateOflastWachedMovie = prompt("На сколько его оцените?","");
+    if(lastWachedMovie != null || lastWachedMovie != "" || lastWachedMovie.length < 50 || RateOflastWachedMovie != null || RateOflastWachedMovie != "") {
+        personalMovieDB.movies[lastWachedMovie] = RateOflastWachedMovie;
+    } else {
+        console.log("Название или длина фильма слишком короткие");
+        i--;
+    }
+    
+}
+
 console.log(personalMovieDB);
